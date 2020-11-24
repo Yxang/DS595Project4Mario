18cd731609e66f21d0114d096b9bc424431673e7
Xinyuan Yang <yangxinyuan100@gmail.com>
2020-11-24 09:41:10 +0800

    Add Xinyuan's DQN agent

--- train.py
+++ train.py
@@ -2,8 +2,9 @@ import mario_wrapper
 from mellowmax_agent import Agent_MM
 from dqn_agent import Agent_DQN
 from ddqn_agent import Agent_DDQN
+from dqn_agent_y import Agent_DQN_y
 
 env = mario_wrapper.create_env()
-agent = Agent_DDQN(env)
+agent = Agent_DQN_y(env)
 agent.train()
 env.close()
