ddf3dae375640bb1cf604bc272e0d9d00a937c5a
alicekenway <45212783+alicekenway@users.noreply.github.com>
2020-11-27 13:35:29 -0600

    Add files via upload

--- mario_wrapper.py
+++ mario_wrapper.py
@@ -26,32 +26,31 @@ class set_reward(Wrapper):
     def _init_(self,env=None):
         super(set_reward,self).__init__(env)
         self.observation_space = Box(low=0, high=255, shape=(1, 84, 84))
-
+        
         self.curr_score = 0
-
+        
 
     def step(self,action):
         state, reward, done, info = self.env.step(action)
         state = transfer_frame(state)
-
-        #reward += (info["score"] - self.curr_score) / 40
-        #self.curr_score = info["score"]
-
-
-        #reward += max(info['x_pos'] - self.x_pos,0)
-        #self.x_pos = info['x_pos']
-
+        
+        reward += (info["score"] - self.curr_score) / 40
+        self.curr_score = info["score"]
+        
+        if self.curr_time > info["time"]:
+            reward = reward + info["time"]-self.curr_time
+            self.curr_time = info["time"]
 
         if done:
             if info["flag_get"]:
                 reward += 50
             else:
                 reward -= 50
-        return state, reward / 5., done, info
+        return state, reward / 10., done, info
 
     def reset(self):
         self.curr_score = 0
-        self.x_pos = 0
+        self.curr_time = 400
         return transfer_frame(self.env.reset())
 
 #Like breakout, merge 4 (1,84,84) frames into 1 (4,84,84), clip the reward between -15 and 15
@@ -63,14 +62,16 @@ class CustomState(Wrapper):
         self.skip = skip
 
     def step(self, action):
+        total_reward = 0
         states = []
         state, reward, done, info = self.env.step(action)
-        total_reward = reward
         for _ in range(self.skip):
-            states.append(state)
             if not done:
                 state, reward, done, info = self.env.step(action)
                 total_reward += reward
+                states.append(state)
+            else:
+                states.append(state)
         states = np.concatenate(states, 0)[None, :, :, :]
         return states.astype(np.float32), np.clip(total_reward,-15,15), done, info
 
@@ -89,9 +90,27 @@ class CustomState(Wrapper):
 
 def create_env(world=1,stage=1,action_type=SIMPLE_MOVEMENT):
 
-    env = gym_super_mario_bros.make('SuperMarioBros-{}-{}-v2'.format(world,stage))
+    env = gym_super_mario_bros.make('SuperMarioBros-{}-{}-v0'.format(world,stage))
     env = JoypadSpace(env, action_type)
     env = set_reward(env)
     env = CustomState(env)
     #print(env.skip)
     return env
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
